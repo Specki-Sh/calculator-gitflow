39a32426c218db75747d7c7f3509dc0e58ae2ea9
Yusup <khemraev1@gmail.com>
2024-08-30 16:44:35 +0500

    fix: added to cli subtract

--- calculator/__main__.py
+++ calculator/__main__.py
@@ -1,7 +1,8 @@
 from calculator.cli import CLI
+from calculator.operations import subtract
 
 if __name__ == "__main__":
-    opt = {}
+    opt = {'subtract': subtract}
 
     cli = CLI(opt)
     cli.run()
