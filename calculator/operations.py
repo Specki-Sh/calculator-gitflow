ebdd19f1346b9ccf9c61da3be35ce21f54752979
Shakhzod Yusupov <YuShakhzod@gmail.com>
2024-08-30 11:11:48 +0500

    fix: raise error if arguments can't be converted to float

--- calculator/operations.py
+++ calculator/operations.py
@@ -1,3 +1,7 @@
-def add(a: int, b: int) -> int:
 def add(a: float, b: float) -> float:
+    try:
+        a = float(a)
+        b = float(b)
+    except ValueError:
+        raise TypeError("arguments must be numbers")
     return a + b
