a9555bc6e9b9daf04bbc9b0e07c364bf561c37f8
Sulton 苏文通 <abdukadirov048@gmail.com>
2024-09-02 10:27:05 +0500

    add multiply

--- calculator/__main__.py
+++ calculator/__main__.py
@@ -1,8 +1,11 @@
 from calculator.cli import CLI
-from calculator.operations import subtract
+from calculator.operations import subtract, multiply
 
 if __name__ == "__main__":
-    opt = {'subtract': subtract}
+    opt = {
+        'subtract': subtract,
+        'multiply': multiply
+    }
 
     cli = CLI(opt)
     cli.run()
