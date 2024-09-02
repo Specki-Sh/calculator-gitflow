575842595f4d639f604fd720ee9ba2ed946c75c3
Sulton 苏文通 <abdukadirov048@gmail.com>
2024-09-02 10:36:07 +0500

    add multiply

--- calculator/operations.py
+++ calculator/operations.py
@@ -5,3 +5,12 @@ def subtract(a: float, b: float) -> float:
     except ValueError:
         raise TypeError("arguments must be numbers")
     return a - b
+
+
+def multiply(a: float, b: float) -> float:
+    try:
+        a = float(a)
+        b = float(b)
+    except ValueError:
+        raise TypeError("arguments must be numbers")
+    return a * b
