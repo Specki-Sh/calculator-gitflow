e81f824aef6f048b49c9369ba4c8075c1e1b4dba
Sulton 苏文通 <abdukadirov048@gmail.com>
2024-09-02 10:34:57 +0500

    Revert "add multiply"
    This reverts commit a9555bc6e9b9daf04bbc9b0e07c364bf561c37f8.

--- calculator/__main__.py
+++ calculator/__main__.py
@@ -1,11 +1,8 @@
 from calculator.cli import CLI
-from calculator.operations import subtract, multiply
+from calculator.operations import subtract
 
 if __name__ == "__main__":
-    opt = {
-        'subtract': subtract,
-        'multiply': multiply
-    }
+    opt = {'subtract': subtract}
 
     cli = CLI(opt)
     cli.run()
--- calculator/operations.py
+++ calculator/operations.py
@@ -5,12 +5,3 @@ def subtract(a: float, b: float) -> float:
     except ValueError:
         raise TypeError("arguments must be numbers")
     return a - b
-
-
-def multiply(a: float, b: float) -> float:
-    try:
-        a = float(a)
-        b = float(b)
-    except ValueError:
-        raise TypeError("arguments must be numbers")
-    return a * b
