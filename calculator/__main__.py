66d8efb1dd99c75cc125098b59982847e53a05db
Shakhzod Yusupov <YuShakhzod@gmail.com>
2024-09-02 14:04:05 +0500

    style: call from module for operations funcs

--- calculator/__main__.py
+++ calculator/__main__.py
@@ -1,12 +1,11 @@
 from calculator import operations
 from calculator.cli import CLI
-from calculator.operations import subtract, multiply
 
 if __name__ == "__main__":
     opt = {
         'add': operations.add,
-        'subtract': subtract,
-        'multiply': multiply,
+        'subtract': operations.subtract,
+        'multiply': operations.multiply,
     }
 
     cli = CLI(opt)
